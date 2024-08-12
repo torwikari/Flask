0311244e0c9863c445210de7e178bade45a09a68
Torwikari <torwikari@gmail.com>
2024-08-12 14:33:58 +0200

    added forms

--- flask_blog/flaskblog.py
+++ flask_blog/flaskblog.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+
+app.config["SECRET_KEY"] = "0b8749e94baa815f9a13854edcfe84f5"
 
 posts = [
     {
@@ -27,6 +30,21 @@ def home():
 @app.route("/about")
 def aboutd():
     return render_template("about.html", title="About")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f"Account created for {form.username.data}!", "success")
+        return redirect(url_for("home"))
+    return render_template("register.html", title="Register", form=form)
+
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template("login.html", title="Login", form=form)
 
 
 if __name__ == "__main__":
